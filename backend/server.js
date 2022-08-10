403513401526c96267a68fe57c9c4d9a114cfa30
Crash5b <crash5band@gmail.com>
2022-08-10 12:33:04 +0200

    Error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const errorHandler = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
@@ -10,5 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/notes', require('./routes/noteRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
